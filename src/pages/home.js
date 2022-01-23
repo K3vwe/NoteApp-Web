0bcc23495eddf24b64f3eccc1ffd9acc05118780
k3vwe <rukkimax@gmail.com>
2022-01-23 23:38:20 +0100

    Link pages components together

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 function Home() {
 
@@ -6,6 +7,11 @@ function Home() {
         <div>
             <h1>This is the Home Page</h1>
             <p>Data Base Notes Saved</p>
+            {/* Add a couple of components to link to */}
+            <ul>
+                <li> <Link to="/mynotes">MyNotes</Link> </li>
+                <li> <Link to="/favorites">Favorites</Link> </li>
+            </ul>
         </div>
     );
 }
