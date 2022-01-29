a090398617947d6c22fe72e673f9ed1499727440
k3vwe <rukkimax@gmail.com>
2022-01-29 16:30:42 +0100

    Configure Apollo Client for React Note App

--- src/App.js
+++ src/App.js
@@ -1,14 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
 import GlobalStyle from './components/GlobalStyle';
 import Pages from './pages';
 
+const client = new ApolloClient({
+  uri: process.env.API_URI,
+  cache: new InMemoryCache(),
+  connectToDevTools: true
+});
+
 function App() {
-  
   return (
     <div className="App">
-      <GlobalStyle />
-      <Pages />
+      <ApolloProvider client={ client }>
+        <GlobalStyle />
+        <Pages />
+      </ApolloProvider>
     </div>
   );
 }
--- src/pages/home.js
+++ src/pages/home.js
@@ -1,13 +1,45 @@
 import React from 'react';
 import Button from '../components/Button';
 
+// import the required libraries to query server
+import { useQuery, gql } from '@apollo/client';
+
+// GraphQL query stored as a variable
+const GET_NOTES = gql`
+    query($cursor: String) {
+        noteFeed(cursor: $cursor) {
+            notes {
+                id
+                createdAt
+                content
+                favoriteCount
+                author {
+                id
+                email
+                username
+                }
+            }
+            hasNextPage
+            cursor
+        }
+    }
+`;
+
 function Home() {
+
+    // QueryHook
+    const { loading, error, data } = useQuery(GET_NOTES);
+
+    // if data is loading, return a loading message
+    if(loading) return <h4>Data Loading...</h4>
+
+    // if error occurs while loading data, output an error message
+    if(error) return <h4>Error Loading Message...</h4>
 
     return (
         <div>
-            <h1>This is the Home Page</h1>
-            <p>Home Page</p>
-            <Button>Click Me</Button>
+            {console.log(data)}
+            <h4>Data Loaded</h4>
         </div>
     );
 }
