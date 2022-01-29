67b07242f2517cf3a295c511b1fe7525fcdfceae
k3vwe <rukkimax@gmail.com>
2022-01-29 21:08:27 +0100

    Add styling for each note in homepage

--- src/components/Note.js
+++ src/components/Note.js
@@ -1,12 +1,42 @@
 import React from 'react';
 import ReactMarkdown from 'react-markdown';
+import styled from 'styled-components';
+import { format, parseISO } from 'date-fns';
+
+const StyledNote = styled.article`
+    max-width: 100%;
+    margin: 0 auto;
+`;
+
+const MetaData = styled.div`
+    @media (min-width: 500px){
+        display: flex;
+        align-items: center;
+    }
+`;
+
+const MetaInfo = styled.div`
+    padding-right: 1em;
+`;
+
+const UserAction = styled.div`
+    margin-left: auto;
+`;
 
 function Note({ note }) {
     return(
-        <div>
-            {note.author.username} {note.createdAt} {note.favoriteCount} {' '}
-            <ReactMarkdown children={note.content} />
-        </div>
+        <StyledNote>
+            <MetaData>
+                <MetaInfo>
+                    <em>by: </em>{note.author.username}<br />
+                    {format(parseISO(note.createdAt), 'do MMM yyyy')}
+                </MetaInfo>
+                <UserAction>
+                    <h4>Favorites: {note.favoriteCount}</h4> 
+                </UserAction>
+                <ReactMarkdown children={note.content} />
+            </MetaData>
+        </StyledNote>
     )
 }
 
--- src/components/NoteFeed.js
+++ src/components/NoteFeed.js
@@ -1,15 +1,24 @@
 import React from 'react';
 import Note from './Note';
+import styled from 'styled-components';
+
+const NoteWrapper = styled.div`
+    max-width: 800px;
+    margin: 0 auto;
+    margin-bottom: 2em;
+    padding-bottom: 2em;
+    border-bottom: 1px solid #f5f4f0;
+`;
 
 function NoteFeed({ notes }){
     return(
-        <div>
+        <NoteWrapper>
             {notes.map( note => (
                 <div key={note.id}>
                     <Note note={note} />
                 </div>
             ))}
-        </div>
+        </NoteWrapper>
     );
 }
 
